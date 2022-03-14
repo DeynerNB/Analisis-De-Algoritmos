c07a8703ca86542cf3455763e4b570fe3c9d0efb
DeynerNB <71191734+DeynerNB@users.noreply.github.com>
2022-03-14 00:38:42 -0600

    Update main.cpp

--- Quiz #4 - Quiz #5/main.cpp
+++ Quiz #4 - Quiz #5/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int elementoMayor_Indice(int array[], int sizeArray) {
     int mayor = -1;
-    int indice = 0;
+    int indice = -1;
 
     for (int i = 0; i < sizeArray; i++) {
         if (array[i] > mayor) {
@@ -15,7 +15,7 @@ int elementoMayor_Indice(int array[], int sizeArray) {
     return indice;
 }
 
-int printe(int array[], int n) {
+void printe(int array[], int n) {
     for (int i = 0; i < n; i++) {
         cout << array[i] << endl;
     }
@@ -25,13 +25,13 @@ int costo(int N, int M, int x[], int y[]) {
 
     if (N == 1 && M == 1)
         return 0;
-    
+
     int mayorX = elementoMayor_Indice(x, N-1);
     int mayorY = elementoMayor_Indice(y, M-1);
     int coste1 = 0;
     int coste2 = 0;
 
-    if (M <= 1 || x[mayorX] > y[mayorY]) {
+    if ((mayorY == -1) || ((mayorX >= 0 && mayorY >= 0) && (x[mayorX] > y[mayorY]))) {
 
         coste1 = costo(mayorX + 1, M, x, y);
         coste2 = costo(N - (mayorX + 1), M, x + (mayorX + 1), y);
@@ -49,13 +49,10 @@ int costo(int N, int M, int x[], int y[]) {
 
 int main() {
 
-    int x[] = {1,2,100,4,5,6,7,8,9};
+    int x[] = {2, 7, 8};
+    int y[] = {4, 3, 9};
 
-    int mayorX = elementoMayor_Indice(x, 9);
-
-    printe(x, mayorX + 1);
-    cout << endl;
-    printe(x + (mayorX + 1), 9 - (mayorX + 1));
+    cout << costo(4, 4, x, y) << endl;
 
     return 0;
 }
