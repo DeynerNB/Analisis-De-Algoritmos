76652385f0f3bb483dfaf23b87bdf08c9e28c75c
DeynerNB <deynernavarrob@gmail.com>
2022-03-13 22:44:08 -0600

    Caso #2

--- Caso #2/insertionSort.cpp
+++ Caso #2/insertionSort.cpp
@@ -4,42 +4,178 @@
 
 using namespace std;
 
+int ANALISIS_intercambios;
+int ANALISIS_operaciones;
+int ANALISIS_asignaciones;
+int ANALISIS_comparaciones;
+
+void generarArray(vector<int> &array, int n) {
+	for (int i = 0; i < n; i++) {
+		array.push_back(rand() % 200 + 1);
+	}
+}
+
+void generarArrayMC(vector<int> &array, int n) {
+	for (int i = 0; i < n; i++) {
+		array.push_back(i);
+	}
+}
+void generarArrayPC(vector<int> &array, int n) {
+	for (int i = 0; i < n; i++) {
+		array.push_back(n - i);
+	}
+}
+void mostrarArray(vector<int> array) {
+	cout << "[";
+	for (int e : array) {
+		cout << e << ", ";
+	}
+	cout << "]";
+}
+
+void reiniciarContadores() {
+	ANALISIS_intercambios = 0;
+	ANALISIS_operaciones = 0;
+	ANALISIS_asignaciones = 0;
+	ANALISIS_comparaciones = 0;
+}
+
 void insertionSort(vector<int> &array) {
 
 	int arraySize = array.size();
+	ANALISIS_asignaciones++;
 
+	ANALISIS_comparaciones++;
 	if (arraySize > 1) {
 
 		int pointer1 = 0;
 		int pointer2 = 1;
 		int aux1 = pointer1;
 		int aux2 = pointer2;
+		ANALISIS_asignaciones += 4;
 
 		while(pointer2 < arraySize) {
+			ANALISIS_comparaciones++;
+
+			ANALISIS_comparaciones += 3;
+			ANALISIS_operaciones += 2;
 			if (aux1 >= 0 && array[aux1] > array[aux2]) {
 
 				int saveKey = array[aux1];
 				array[aux1] = array[aux2];
 				array[aux2] = saveKey;
+				ANALISIS_asignaciones += 3;
+				ANALISIS_operaciones += 4;
+				ANALISIS_intercambios += 2;
 				
 				aux1--;
 				aux2--;
+				ANALISIS_operaciones += 2;
+				ANALISIS_asignaciones += 2;
 				continue;
 			}
 			pointer1++;
 			pointer2++;
 			aux1 = pointer1;
 			aux2 = pointer2;
+			ANALISIS_asignaciones += 4;
+			ANALISIS_operaciones += 2;
 		}
 	}
 }
 
 int main() {
 
-	vector<int> array = {4,3,2,10,12,1,5,6};
+	vector<int> array1;
+	vector<int> array2;
+	vector<int> array3;
+	vector<int> array4;
+	vector<int> array5;
 
-	insertionSort(array);
+	vector<int> arrayMC;
+	vector<int> arrayPC;
 
+	generarArray(array1, 10);
+	generarArray(array2, 10);
+	generarArray(array3, 10);
+	generarArray(array4, 10);
+	generarArray(array5, 10);
+
+	generarArrayMC(arrayMC, 10);
+	generarArrayPC(arrayPC, 10);
+
+
+	reiniciarContadores();
+	insertionSort(array1);
+	cout << "---------------------------------";
+	cout << "\nArray Tamano: " << array1.size();
+	cout << "\nIntercambio: " << ANALISIS_intercambios;
+	cout << "\nOperaciones: " << ANALISIS_operaciones;
+	cout << "\nAsignaciones: " << ANALISIS_asignaciones;
+	cout << "\nComparaciones: " << ANALISIS_comparaciones;
+	cout << "\n---------------------------------\n\n";
+
+	reiniciarContadores();
+	insertionSort(array2);
+	cout << "---------------------------------";
+	cout << "\nArray Tamano: " << array2.size();
+	cout << "\nIntercambio: " << ANALISIS_intercambios;
+	cout << "\nOperaciones: " << ANALISIS_operaciones;
+	cout << "\nAsignaciones: " << ANALISIS_asignaciones;
+	cout << "\nComparaciones: " << ANALISIS_comparaciones;
+	cout << "\n---------------------------------\n\n";
+
+	reiniciarContadores();
+	insertionSort(array3);
+	cout << "---------------------------------";
+	cout << "\nArray Tamano: " << array3.size();
+	cout << "\nIntercambio: " << ANALISIS_intercambios;
+	cout << "\nOperaciones: " << ANALISIS_operaciones;
+	cout << "\nAsignaciones: " << ANALISIS_asignaciones;
+	cout << "\nComparaciones: " << ANALISIS_comparaciones;
+	cout << "\n---------------------------------\n\n";
+
+	reiniciarContadores();
+	insertionSort(array4);
+	cout << "---------------------------------";
+	cout << "\nArray Tamano: " << array4.size();
+	cout << "\nIntercambio: " << ANALISIS_intercambios;
+	cout << "\nOperaciones: " << ANALISIS_operaciones;
+	cout << "\nAsignaciones: " << ANALISIS_asignaciones;
+	cout << "\nComparaciones: " << ANALISIS_comparaciones;
+	cout << "\n---------------------------------\n\n";
+
+	reiniciarContadores();
+	insertionSort(array5);
+	cout << "---------------------------------";
+	cout << "\nArray Tamano: " << array5.size();
+	cout << "\nIntercambio: " << ANALISIS_intercambios;
+	cout << "\nOperaciones: " << ANALISIS_operaciones;
+	cout << "\nAsignaciones: " << ANALISIS_asignaciones;
+	cout << "\nComparaciones: " << ANALISIS_comparaciones;
+	cout << "\n---------------------------------\n\n";
+
+	reiniciarContadores();
+	cout << "---------------------------------\n";
+	cout << "ArrayMC: ";	mostrarArray(arrayMC);
+	insertionSort(arrayMC);
+	cout << "\nArray Tamano: " << arrayMC.size();
+	cout << "\nIntercambio: " << ANALISIS_intercambios;
+	cout << "\nOperaciones: " << ANALISIS_operaciones;
+	cout << "\nAsignaciones: " << ANALISIS_asignaciones;
+	cout << "\nComparaciones: " << ANALISIS_comparaciones;
+	cout << "\n---------------------------------\n\n";
+
+	reiniciarContadores();
+	cout << "---------------------------------\n";
+	cout << "ArrayPC: ";	mostrarArray(arrayPC);
+	insertionSort(arrayPC);
+	cout << "\nArray Tamano: " << arrayPC.size();
+	cout << "\nIntercambio: " << ANALISIS_intercambios;
+	cout << "\nOperaciones: " << ANALISIS_operaciones;
+	cout << "\nAsignaciones: " << ANALISIS_asignaciones;
+	cout << "\nComparaciones: " << ANALISIS_comparaciones;
+	cout << "\n---------------------------------\n\n";
 
 	return 0;
 }
--- Caso #2/quickSort.cpp
+++ Caso #2/quickSort.cpp
@@ -3,64 +3,156 @@
 
 using namespace std;
 
+// 0 Estatico - 1 Aleatorio
+#define PIVOT_TYPE 0
 
-vector<int> QuickSort_StaticPivot(vector<int> array) {
-    if (array.size() > 1) {
+int ANALISIS_intercambios;
+int ANALISIS_operaciones;
+int ANALISIS_asignaciones;
+int ANALISIS_comparaciones;
+int ANALISIS_llamadas;
+int ANALISIS_retornos;
 
-        int pivot = array[array.size() - 1];
+bool firstPivot = true;
+
+int getPivot(vector<int> array, int n) {
+    if (PIVOT_TYPE)
+        return array[rand() % n];
+    else
+        return array[n - 1];
+}
+void generarArray(vector<int> &array, int n) {
+    for (int i = 0; i < n; i++) {
+        array.push_back(rand() % 200 + 1);
+    }
+}
+void reiniciarContadores() {
+    ANALISIS_intercambios = 0;
+    ANALISIS_operaciones = 0;
+    ANALISIS_asignaciones = 0;
+    ANALISIS_comparaciones = 0;
+    ANALISIS_llamadas = 0;
+    ANALISIS_retornos = 0;
+    firstPivot = true;
+}
+void mostrarArray(vector<int> array) {
+    cout << "[";
+    for (int e : array) {
+        cout << e << ", ";
+    }
+    cout << "]";
+}
+
+
+vector<int> QuickSort(vector<int>& array) {
+
+    int arraySize = array.size();
+    ANALISIS_asignaciones++;
+    ANALISIS_llamadas++;
+
+    ANALISIS_comparaciones++;
+    if (arraySize > 1) {
+
+        int pivot = getPivot(array, arraySize);
+        ANALISIS_asignaciones++;
+        ANALISIS_llamadas+=3;
+
+        if (firstPivot) {
+            cout << "\nPivote Inicial: " << pivot;
+            firstPivot = false;
+        }
 
         vector<int> leftSide;
         vector<int> rightSide;
 
-        for (auto elem = array.begin(); elem != (array.end() - 1); ++elem) {
+        ANALISIS_asignaciones++;
+        for (auto elem = array.begin(); elem != array.end(); ++elem) {
+            ANALISIS_comparaciones++;
+            ANALISIS_asignaciones++;
+            ANALISIS_operaciones += 1;
+
+            ANALISIS_comparaciones++;
+            ANALISIS_llamadas+=2;
             if (*elem > pivot)
                 rightSide.push_back(*elem);
-            else
+            else if(*elem < pivot)
                 leftSide.push_back(*elem);
         }
 
-        leftSide = QuickSort_StaticPivot(leftSide);
-        rightSide = QuickSort_StaticPivot(rightSide);
+        ANALISIS_asignaciones += 2;
+        ANALISIS_llamadas += 2;
+        leftSide = QuickSort(leftSide);
+        rightSide = QuickSort(rightSide);
 
+        ANALISIS_llamadas += 2;
         leftSide.push_back(pivot);
 
+        ANALISIS_llamadas += 7;
         leftSide.insert(leftSide.end(), rightSide.begin(), rightSide.end());
+
+        ANALISIS_asignaciones++;
         array = leftSide;
     }
-    return array;
-}
-vector<int> QuickSort_RandomPivot(vector<int> array) {
-    if (array.size() > 1) {
 
-        int pivotPosition = rand() % array.size();
-        int pivot = array[pivotPosition];
-
-        vector<int> leftSide;
-        vector<int> rightSide;
-
-        for (int i = 0; i < array.size(); i++) {
-            if (i != pivotPosition) {
-                if (array[i] > pivot)
-                    rightSide.push_back(array[i]);
-                else
-                    leftSide.push_back(array[i]);
-            }
-        }
-
-        leftSide = QuickSort_RandomPivot(leftSide);
-        rightSide = QuickSort_RandomPivot(rightSide);
-
-        leftSide.push_back(pivot);
-
-        leftSide.insert(leftSide.end(), rightSide.begin(), rightSide.end());
-        array = leftSide;
-    }
+    ANALISIS_retornos++;
     return array;
 }
 
 int main() {
 
-    V = QuickSort_StaticPivot(V);
+    vector<int> arrayS_1 = {77, 75, 23, 43, 55, 12, 64, 44};
+    vector<int> arrayS_2 = {10, 20, 30, 40, 50, 60, 70, 80};
+    vector<int> arrayS_3 = {80, 70, 60, 50, 40, 30, 20, 10};
+
+    if (PIVOT_TYPE)
+        cout << " >> Pivote Aleatorio\n";
+    else
+        cout << " >> Pivote Fijo\n";
+
+    reiniciarContadores();
+    cout << "---------------------------------\n";
+    cout << "Array Original: "; mostrarArray(arrayS_1);
+    arrayS_1 = QuickSort(arrayS_1);
+    cout << "\nArray Tamano: " << arrayS_1.size();
+    cout << "\nIntercambio: " << ANALISIS_intercambios;
+    cout << "\nOperaciones: " << ANALISIS_operaciones;
+    cout << "\nAsignaciones: " << ANALISIS_asignaciones;
+    cout << "\nComparaciones: " << ANALISIS_comparaciones;
+    cout << "\nLlamadas: " << ANALISIS_llamadas;
+    cout << "\nRetornos: " << ANALISIS_retornos;
+    cout << "\nArray Ordenado: "; mostrarArray(arrayS_1);
+    cout << "\n---------------------------------\n\n";
+
+    reiniciarContadores();
+    cout << "---------------------------------\n";
+    cout << "Array Original: "; mostrarArray(arrayS_2);
+    arrayS_2 = QuickSort(arrayS_2);
+    cout << "\nArray Tamano: " << arrayS_2.size();
+    cout << "\nIntercambio: " << ANALISIS_intercambios;
+    cout << "\nOperaciones: " << ANALISIS_operaciones;
+    cout << "\nAsignaciones: " << ANALISIS_asignaciones;
+    cout << "\nComparaciones: " << ANALISIS_comparaciones;
+    cout << "\nLlamadas: " << ANALISIS_llamadas;
+    cout << "\nRetornos: " << ANALISIS_retornos;
+    cout << "\nArray Ordenado: "; mostrarArray(arrayS_2);
+    cout << "\n---------------------------------\n\n";
+
+    reiniciarContadores();
+    cout << "---------------------------------\n";
+    cout << "Array Original: "; mostrarArray(arrayS_3);
+    arrayS_3 = QuickSort(arrayS_3);
+    cout << "\nArray Tamano: " << arrayS_3.size();
+    cout << "\nIntercambio: " << ANALISIS_intercambios;
+    cout << "\nOperaciones: " << ANALISIS_operaciones;
+    cout << "\nAsignaciones: " << ANALISIS_asignaciones;
+    cout << "\nComparaciones: " << ANALISIS_comparaciones;
+    cout << "\nLlamadas: " << ANALISIS_llamadas;
+    cout << "\nRetornos: " << ANALISIS_retornos;
+    cout << "\nArray Ordenado: "; mostrarArray(arrayS_3);
+    cout << "\n---------------------------------\n\n";
+
+
+
 
     return 0;
 }
