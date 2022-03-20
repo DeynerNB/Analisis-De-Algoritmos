0749b8a49900e7e525a90f66bf838be0a56ffb80
DeynerNB <deynernavarrob@gmail.com>
2022-03-19 19:16:57 -0600

    Caso #2

--- Caso #2/freeText.cpp
+++ Caso #2/freeText.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <time.h>
 using namespace std;
 
+map<string, int> prefixPatternMap;
 
+void initPrefixPatternMap(string pattern) {
+	
+	int patternLenght = pattern.length();
 
+	for (int i = 0; i < (patternLenght - 1); i++) {
+		prefixPatternMap.insert(make_pair(pattern.substr(0, i+1), i));
+	}
+}
 
 int getCharIndex(string pattern, char letter, int sizePattern) {
 	int index = -1;
@@ -19,47 +29,48 @@ int Boyer_Moore_BadChar(string text, string pattern, int pointerPattern, int poi
 	int badChar_Index = getCharIndex(pattern, text[pointerText], pointerPattern);
 
 	if (badChar_Index != -1) {
-		return pointerPattern - badChar_Index;
+		return shiftPattern + (pointerPattern - badChar_Index);
 	}
 	else {
-		return shiftPattern + pattern.length();
+		return shiftPattern + pointerPattern + 1;
 	}
 }
 
-int Boyer_Moore_GoodSufx(string text, string pattern, int pointerPattern, int pointerText, int shiftPattern) {
+int Boyer_Moore_GoodSufx(string text, string pattern, int pointerPattern, int pointerText, int shiftPattern) {	
+
 	if (shiftPattern <= 1) {
 		return shiftPattern;
 	}
 	else {
-
 		string subStringText = text.substr((pointerText + 1), shiftPattern);
-		int aux_I;
+		string subPattern;
 
-		for (int i = 0; i < shiftPattern; i++) {
-			aux_I = i;
+		// Check for sufix inside the pattern
+		for (int i = 0; i <= pointerPattern; i++) {
+			subPattern = pattern.substr((pointerPattern - i), shiftPattern);
+			if (subPattern == subStringText)
+				return shiftPattern + (i + 1);
+		}
 
-			for (int j = 0; j <= (pointerPattern + i + 1) && (aux_I < subStringText.length()); j++) {
-				if (subStringText[aux_I] == pattern[j]) {
-					aux_I++;
-				}
-				else {
-					aux_I = i;
-				}
-			}
-
-			if (aux_I >= subStringText.length()) {
-				return (subStringText.length() - i);
-			}
+		// Check for sufix of sufix in prefix of pattern
+		while(subStringText.length() > 1) {
+			subStringText.erase(0, 1);
+			if (prefixPatternMap.find(subStringText) != prefixPatternMap.end())
+				return shiftPattern + pointerPattern + 1;
 		}
 
 		return -1;
 	}
+
 }
+
 
 int Boyer_Moore_Text(string text, string pattern) {
 
 	if (pattern.length() > text.length())
 		return 0;
+
+	initPrefixPatternMap(pattern);
 
 	int pointerPattern = pattern.length() - 1;
 	int pointerText = pointerPattern;
@@ -89,7 +100,7 @@ int Boyer_Moore_Text(string text, string pattern) {
 			);
 
 			pointerPattern += shiftPattern;
-			pointerText += (shiftPattern + numShift);
+			pointerText += numShift;
 
 			shiftPattern = 0;
 		}
@@ -99,7 +110,7 @@ int Boyer_Moore_Text(string text, string pattern) {
 }
 
 void readFile(string *text) {
-	ifstream file("text2.txt");
+	ifstream file("text.txt");
 
 	cout << "Opening file...\n";
 	if (file.is_open()) {
@@ -121,14 +132,42 @@ void readFile(string *text) {
 int main() {
 
 	string text;
-	string pattern = "moving";
+	string pattern = "god";
+	int result = 0;
+	unsigned t0, t1;
+
 	readFile(&text);
 
-	cout << text.length() << endl;
+	// Cambiar texto a minusculas
+	cout << "\nFile lower case conversion initializing...\n";
+	t0 = clock();
+	for (int i = 0; i < text.length(); i++)
+		text[i] = tolower(text[i]);
+	t1 = clock();
+	cout << "File lower case conversion finished\n";
+	cout << "Total time conversion: " << double(t1 - t0)/CLOCKS_PER_SEC << endl;
 
-	cout << "\nIniciando proceso...\n";
-	cout << Boyer_Moore_Text(text, pattern) << endl;
-	cout << "Proceso finalizado...\n";
+	int limitSize = text.length()/1000;
+	string subText;
+
+	cout << "\nFile procesing initializing...\n";
+	t0 = clock();
+	for (int i = 0; i < text.length(); i += limitSize) {
+		subText = text.substr(i, limitSize);
+
+		while (text[i + limitSize] != ' ' && text[i + limitSize] != '\0') {
+			subText += text[i + limitSize];
+			i++;
+		}
+
+		result += Boyer_Moore_Text(subText, pattern);
+	}
+	t1 = clock();
+	cout << "File procesing finished\n";
+	cout << "Time Processing: " << double(t1 - t0)/CLOCKS_PER_SEC << endl;
+
+
+	cout << "\nTotal apears of '" << pattern << "': " << result << endl;
 
 
 	return 0;
